c2b1e5f1cb6b7073c767d646f0660302f034935a
arthu <arthu@ARTHUR>
2024-07-17 20:06:43 -0300

    Adicionar arquivos de projeto.

---
+++ if_else/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace if_else
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite sua idade");
+            int idade = int.Parse(Console.ReadLine());
+
+            if (idade >= 0 && idade <= 11)
+            {
+                Console.WriteLine("Você é uma criança");
+
+            }
+
+            else if (idade >= 12 && idade <= 18)
+            {
+                Console.WriteLine("Você é um adolecente !");
+            }
+            else if (idade >= 19 && idade <= 60)
+            {
+                Console.WriteLine("Você é adulto ! ");
+
+            }
+
+
+            else
+            {
+                Console.WriteLine("Você é idoso");
+            }
+
+
+            Console.ReadLine();
+
+
+
+
+        }
+    }
+}
